cc2287e7c50f56b8217a77d3cfaf96eb5c34efc1
bay007 <tnt.galicia@gmail.com>
2017-04-08 18:45:05 -0500

    -Agregadas dependendias ESLInt -Codigo arreglado en base a Eslint

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import './App.css';
 
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state={
       alertIsOpen:false,
       collapseIsOpen:false,
       dialogIsOpen:false,
       isCheckboxCheked:false,
-      radioGroupselectedValue:null,
-      
-    }
+      radioGroupselectedValue:null
+    },
+
     this.alertDialog=this.alertDialog.bind(this);
     this.AcceptcloseAlert=this.AcceptcloseAlert.bind(this);
     this.CancelcloseAlert=this.CancelcloseAlert.bind(this);
@@ -29,56 +29,56 @@ class App extends Component {
   }
 
 refHandlers = {
-  toaster: (ref) => {this.toaster = ref}
+  toaster: (ref) => {this.toaster = ref;}
 }
 
 onChangeRadioGroup=(FormEvent)=>{
-  this.setState({radioGroupselectedValue:FormEvent.target.value})
-  this.toaster.show({message:"RadioGroup: "+FormEvent.target.value})
+  this.setState({radioGroupselectedValue:FormEvent.target.value});
+  this.toaster.show({message:"RadioGroup: "+FormEvent.target.value});
 }
 
 onChangeSwitch=(FormEvent)=>{
-  this.toaster.show({message:"SWITCH: "+ FormEvent.target.checked})
+  this.toaster.show({message:"SWITCH: "+ FormEvent.target.checked});
 }
 
 onChangeCheckbox=(FormEvent)=>{
-  this.toaster.show({message:"CHECKBOX: "+ FormEvent.target.checked})
-  this.setState({isCheckboxCheked:!this.state.isCheckboxCheked})
+  this.toaster.show({message:"CHECKBOX: "+ FormEvent.target.checked});
+  this.setState({isCheckboxCheked:!this.state.isCheckboxCheked});
 }
 
 onConfirmEditableText=(string)=>{
   console.log(string);
-  this.toaster.show({message:string})
+  this.toaster.show({message:string});
 }
 
 openDialog=()=>{
-  this.setState({dialogIsOpen:true})
+  this.setState({dialogIsOpen:true});
 }
 
 onCloseDialog=()=>{
-  this.setState({dialogIsOpen:!this.state.dialogIsOpen})
-  console.log("Cerrado")
+  this.setState({dialogIsOpen:!this.state.dialogIsOpen});
+  console.log("Cerrado");
 }
 
 collapseOpenClose=()=>{
-  this.setState( {collapseIsOpen:!this.state.collapseIsOpen} )
+  this.setState( {collapseIsOpen:!this.state.collapseIsOpen} );
 }
 
 alertDialog=()=>{
-  this.setState({alertIsOpen:true})
-  console.log(Classes)
-  console.log(IconClasses)
+  this.setState({alertIsOpen:true});
+  console.log(Classes);
+  console.log(IconClasses);
 }
 
 
 AcceptcloseAlert=()=>{
-  this.setState({alertIsOpen:false})
-  alert("Aceptado")
+  this.setState({alertIsOpen:false});
+  alert("Aceptado");
 }
 
 CancelcloseAlert=()=>{
-  this.setState({alertIsOpen:false})
-  alert("Cancelado")
+  this.setState({alertIsOpen:false});
+  alert("Cancelado");
 }
 
   render() {
@@ -94,13 +94,13 @@ CancelcloseAlert=()=>{
         <p><Button iconName="refresh"  active="true" text="Refresh Activitys" onClick={this.alertDialog} /></p>
         
           <Alert  //http://blueprintjs.com/docs/#core/components/alert
-            isOpen={this.state.alertIsOpen}
-            cancelButtonText="¿Cancelar la operación?"
-            confirmButtonText="Proceder?"
-            iconName={IconClasses.COMPARISON}
-            onCancel={this.CancelcloseAlert}
-            onConfirm={this.AcceptcloseAlert}
-            className="pt-callout pt-icon-info-sign" //*http://blueprintjs.com/docs/#core/components/callout
+              isOpen={this.state.alertIsOpen}
+              cancelButtonText="¿Cancelar la operación?"
+              confirmButtonText="Proceder?"
+              iconName={IconClasses.COMPARISON}
+              onCancel={this.CancelcloseAlert}
+              onConfirm={this.AcceptcloseAlert}
+              className="pt-callout pt-icon-info-sign" //*http://blueprintjs.com/docs/#core/components/callout
           >
             <p>
                 Couldn't create the file because the containing folder doesn't exist anymore.
@@ -119,20 +119,20 @@ CancelcloseAlert=()=>{
           <hr/>
         <p>
           <Button 
-          active
-          text="Mostrar <Dialog/>"
-          onClick={this.openDialog}
+              active
+              text="Mostrar <Dialog/>"
+              onClick={this.openDialog}
           />
           <Dialog /* http://blueprintjs.com/docs/#core/components/dialog */
-            canEscapeKeyClose={true}
-            canOutsideClickClose={true}
-            iconName={IconClasses.AIRPLANE}
-            isOpen={this.state.dialogIsOpen}
-            transitionDuration={150}
-            title="Este es el title de el dialogo"
-            onClose={this.onCloseDialog}
-            isCloseButtonShown={true}
-            inline={false}
+              canEscapeKeyClose={true}
+              canOutsideClickClose={true}
+              iconName={IconClasses.AIRPLANE}
+              isOpen={this.state.dialogIsOpen}
+              transitionDuration={150}
+              title="Este es el title de el dialogo"
+              onClose={this.onCloseDialog}
+              isCloseButtonShown={true}
+              inline={false}
           > 
             <p>[11:53:30] Finished 'typescript-bundle-blueprint' after 769 ms</p>
             <p>[11:53:30] Starting 'typescript-typings-blueprint'...</p>
@@ -144,46 +144,46 @@ CancelcloseAlert=()=>{
         
         
           <EditableText /*http://blueprintjs.com/docs/#core/components/editable-text*/
-            defaultValue="Hola"
-            maxLength={140}
-            minLines={2}
-            maxLines={4}
-            multiline={true}
-            onConfirm={this.onConfirmEditableText}
-            placeholder="Ingrese sus peticiones"
-            confirmOnEnterKey={true}
-          >
-          </EditableText>
+              defaultValue="Hola"
+              maxLength={140}
+              minLines={2}
+              maxLines={4}
+              multiline={true}
+              onConfirm={this.onConfirmEditableText}
+              placeholder="Ingrese sus peticiones"
+              confirmOnEnterKey={true}
+          />
+          
         <hr/>        
         <p>
           <Checkbox /* http://blueprintjs.com/docs/#core/components/forms/checkbox */
-            disabled={false}
-            label="Estudio grabacion"
+              disabled={false}
+              label="Estudio grabacion"
             /*defaultChecked={true}*/
-            checked={this.state.isCheckboxCheked}
-            onChange={this.onChangeCheckbox}
+              checked={this.state.isCheckboxCheked}
+              onChange={this.onChangeCheckbox}
           />
         </p>
         <hr/>
         
           <Switch 
-            onChange={this.onChangeSwitch}
-            label="Este es un Switch"
-            className={Classes.LARGE&&Classes.ALIGN_LEFT}
+              onChange={this.onChangeSwitch}
+              label="Este es un Switch"
+              className={Classes.LARGE&&Classes.ALIGN_LEFT}
           />
         
 
         <hr/>
           <Popover /* http://blueprintjs.com/docs/#core/components/popover */
-            content="contenido de <Popover/>"
-            interactionKind={PopoverInteractionKind.HOVER}
-            useSmartPositioning={false}
-            position={Position.LEFT}
+              content="contenido de <Popover/>"
+              interactionKind={PopoverInteractionKind.HOVER}
+              useSmartPositioning={false}
+              position={Position.LEFT}
           >
             <RadioGroup 
-            label="Parque de diversiones" 
-            selectedValue={this.state.radioGroupselectedValue}
-            onChange={this.onChangeRadioGroup}
+                label="Parque de diversiones" 
+                selectedValue={this.state.radioGroupselectedValue}
+                onChange={this.onChangeRadioGroup}
             >
               <Radio label="Disney Landia" value="1"/>
               <Radio label="Six Flags" value="2"/>
@@ -194,13 +194,13 @@ CancelcloseAlert=()=>{
         <hr/>
         
         <Toaster /* http://blueprintjs.com/docs/#core/components/toast */
-        timeout={100} 
-        iconName={IconClasses.CHAT} 
-        ref={this.refHandlers.toaster}
-        position={Position.TOP_RIGHT}
-        intent={Intent.PRIMARY}
-        canEscapeKeyClear={true}
-        button="Procure toast"
+            timeout={100} 
+            iconName={IconClasses.CHAT} 
+            ref={this.refHandlers.toaster}
+            position={Position.TOP_RIGHT}
+            intent={Intent.PRIMARY}
+            canEscapeKeyClear={true}
+            button="Procure toast"
         />
         </div>
       </div>
